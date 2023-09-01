2037411817faac9921f5b1ca2fd7c6138336ce4a
YVic95 <vika.yevlentieva@gmail.com>
2023-09-01 17:39:40 +0300

    Add block class for handles.block rendering

---
+++ src/app/code/Mageplaza/Demo/Block/OrderTemplate.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Mageplaza\Demo\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class OrderTemplate extends Template  
+{
+    protected $_template = 'Mageplaza_Demo::custom.phtml';
+}
