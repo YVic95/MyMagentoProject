0d0ed23c939d1de46341400316bbdc61924db1ce
YVic95 <vika.yevlentieva@gmail.com>
2023-09-12 14:10:26 +0300

    Implement responsive behavior with  for desktop and mobile views

--- src/app/design/frontend/Vendor/Custom_Theme/Magento_Theme/web/js/custom.js
+++ src/app/design/frontend/Vendor/Custom_Theme/Magento_Theme/web/js/custom.js
@@ -1,1 +1,29 @@
-console.log('Congrats! Script works');
+define([
+    'matchMedia'
+], (mediaCheck) => {
+    return function() {
+        const displayOnDesktop = () => {
+            console.log('Display on Desktop');
+        }
+
+        const displayOnMobile = () => {
+            console.log('Display on Mobile');
+        }
+
+        // This approach triggers logging every time the window width changes
+
+        // const display = () => {
+        //     return window.innerWidth > 767 ? displayOnDesktop() : displayOnMobile()
+        // }
+
+        // display();
+        // window.addEventListener('resize', display);
+
+        mediaCheck({
+            media: '(min-width: 768px)',
+            entry: displayOnDesktop,
+            exit: displayOnMobile
+        })
+    }  
+    
+});
