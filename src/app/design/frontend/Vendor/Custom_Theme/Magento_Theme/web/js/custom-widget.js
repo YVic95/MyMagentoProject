25bc6caa8557dff87047166614b4d08e6a02b742
YVic95 <vika.yevlentieva@gmail.com>
2023-09-25 18:24:35 +0300

    Add comment on how _on() method of widget factory works

--- src/app/design/frontend/Vendor/Custom_Theme/Magento_Theme/web/js/custom-widget.js
+++ src/app/design/frontend/Vendor/Custom_Theme/Magento_Theme/web/js/custom-widget.js
@@ -8,7 +8,11 @@ define([
             // if you need to fetch config use this.options 
             // this.element.on('click.customWidget', () => {
             //     console.log('click on widget');
-            // });  
+            // });
+            
+            // _on() method binds event handlers to the specified element
+            // Event handlers are automatically namespaced and cleaned up on destroy
+            //  If the widget is disabled the event handler is not invoked
             this._on(this.element, {
                 click: () => {
                     console.log('click on widget');
