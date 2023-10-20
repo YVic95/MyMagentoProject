67529f0a1cc2deae368cdbe75a4d4f79d8b30781
YVic95 <vika.yevlentieva@gmail.com>
2023-10-20 15:58:15 +0300

    Send ajax using mage storage utility for showing a customer system messages

--- src/app/design/frontend/Vendor/Custom_Theme/Magento_Catalog/web/js/widget.js
+++ src/app/design/frontend/Vendor/Custom_Theme/Magento_Catalog/web/js/widget.js
@@ -1,12 +1,14 @@
 define([
     'jquery',
     'mage/translate',
+    'mage/url',
+    'Magento_Customer/js/customer-data',
+    'mage/storage',
     'validation',
     'jquery-ui-modules/widget'
-], ($, $t) => {
+], ($, $t, urlBuilder, customerData, storage) => {
     $.widget('vendor.countryValidationWidget', {
         _init() {
-            //console.log(this.element);
             this.adddEventListeners(); 
         },
 
@@ -27,8 +29,65 @@ define([
         },
 
         sendAjax() {
-            console.log('Send ajax');
-        }
+            this.beforeAjax();
+
+            // Send ajax using mage/storage
+            return storage.get(
+                    urlBuilder.build(`rest/all/V1/directory/countries/${this.getCountryId()}`)
+                    ).done(this.onAjaxSuccess.bind(this)
+                    ).fail(this.onAjaxError.bind(this)
+                    ).always(this.afterAjax.bind(this));
+
+            // Or use jQuery $.ajax 
+                   
+            //return $.ajax({
+                //url: urlBuilder.build(`rest/all/V1/directory/countries/${this.getCountryId()}`),
+                //contentType: 'application/json',
+                //dataType: 'json',
+                //beforeSend: this.beforeAjax.bind(this),
+                //complete: this.afterAjax.bind(this), 
+                //success: this.onAjaxSuccess.bind(this),
+                //error: this.onAjaxError.bind(this)
+            //});
+        },
+
+        getCountryId() {
+            return this.element.serializeArray().find(field => field.name === 'country_id').value;
+        },
+
+        beforeAjax() {
+            $('body').trigger('processStart');
+        },
+
+        afterAjax() {
+            $('body').trigger('processStop');
+        },
+
+        onAjaxSuccess(response) {
+            this.showMessage(`You selected this country: ${response.full_name_english}`, type = 'success');
+            this.element.find('.country-input').val('');
+        },
+
+        onAjaxError(response) {
+            this.showMessage(response.responseJSON.message ?? $t('Ooops, something went wrong'));
+        },
+
+        showMessage(text = '', type = 'error') {
+            if(!text) return this;
+
+            const section = customerData.get('messages')() ?? {};
+
+            // Add the new message
+            customerData.set('messages', {
+                messages: [  
+                    {
+                        text,
+                        type
+                    }
+                ]
+            });
+        },
+
     });
 
     return $.vendor.countryValidationWidget;
