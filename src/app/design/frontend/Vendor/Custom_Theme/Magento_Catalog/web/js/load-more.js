9ddb19e66b816bc76eb812d7ee236c37a286a562
YVic95 <vika.yevlentieva@gmail.com>
2023-10-30 18:51:04 +0300

    widget for showing more product items on the same page

--- src/app/design/frontend/Vendor/Custom_Theme/Magento_Catalog/web/js/load-more.js
+++ src/app/design/frontend/Vendor/Custom_Theme/Magento_Catalog/web/js/load-more.js
@@ -1,8 +1,11 @@
 define([
     'jquery',
     'mage/translate',
+    'mage/storage',
+    'mage/apply/main',
+    'catalogAddToCart',
     'jquery-ui-modules/widget'
-], ($, $t) => {
+], ($, $t, storage, main) => {
     $.widget('vendor.loadProductsWidget', {
         options: {
             selectors: {
@@ -24,7 +27,38 @@ define([
         },
 
         getProductsList() {
-            console.log('products from 2 page');
+            this.beforeAjax();
+
+            return storage
+                .get(`${window.location.pathname}/?p=2`)
+                .fail(this.onAjaxError.bind(this))
+                .done(this.onAjaxSuccess.bind(this))
+                .always(this.afterAjax.bind(this));
+        },
+
+        beforeAjax() {
+            $('body').trigger('proccessStart');
+        },
+
+        afterAjax() {
+            $('body').trigger('proccessStop');
+        },
+
+        onAjaxError(response) {
+            throw new Error($t('Sorry, something went wrong. Error status: %1').replace('%1', response.status));
+        },
+
+        onAjaxSuccess(response) {
+            const htmlOfNextPage = $(response);
+            const dynamicItems = htmlOfNextPage.find(`${this.options.selectors.list} .product-item`);
+            const needApplySelector = 'js-need-apply';
+
+            dynamicItems.css('border', '1px solid blue');
+            dynamicItems.addClass(needApplySelector);
+            $(this.options.selectors.list).append(dynamicItems);
+            main.apply();
+
+            $(this.options.selectors.list).find(`${needApplySelector}`).catalogAddToCart().removeClass(needApplySelector);
         }
     });
 
