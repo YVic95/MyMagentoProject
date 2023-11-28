4d82baeec9955df66383c9922ff6f0961d734b56
YVic95 <vika.yevlentieva@gmail.com>
2023-11-28 16:27:01 +0200

    Code clean up

--- src/app/design/frontend/Vendor/UI_Theme/Magento_Theme/web/js/view/observable-test.js
+++ src/app/design/frontend/Vendor/UI_Theme/Magento_Theme/web/js/view/observable-test.js
@@ -1,15 +1,5 @@
 define(['uiComponent', 'ko'], (uiComponent, ko) => {
     return uiComponent.extend({
-        // ko observable approach
-        // defaults: {
-        //     template: 'Magento_Theme/observable-test.html',
-        //     header: ko.observable('Lets test observables!'),
-        //     text: ko.observable('Default paragraph'),
-        //     isActive: ko.observable(true),
-        //     items: ko.observableArray(['item', 'item'])
-        // },
-
-        //approach with magento tracks
         defaults: {
             template: 'Magento_Theme/observable-test.html',
             header: 'Lets test observables!',
@@ -22,32 +12,6 @@ define(['uiComponent', 'ko'], (uiComponent, ko) => {
                 isActive: true,
                 items: true
             }
-        },
-
-        // initObservable() method executes before initialize()
-        initObservable() {
-            this._super();
-
-            // this.items array has additional methods under hood in comparison with standard js array methods
-            console.log([].__proto__); 
-            console.log(this.items.__proto__);
-
-            // there are two ways to retrieve the new value of the keys from defaults obj: 
-            // using ko observable or using observe method in current component context
-            //this.text = ko.observable('text');
-            // this.observe(['text']);
-
-            // track() triggers observe() under hood
-            // this.track(['text', 'header', 'isActive', 'items'])
-            
-            // It is important to return context, cause initObservable() method is a part of chain methods:
-            // this._super()
-            //     .initObservable()
-            //     .initModules()
-            //     .initStatefull()
-            //     .initLinks()
-            //     .initUnique();
-            return this;
         },
 
         //entry point of the component
