efb1eb3f4d8a3f06472d25e26a9bea3cafd4a82b
YVic95 <vika.yevlentieva@gmail.com>
2023-12-13 17:09:25 +0200

    Add sorting the child components using getRegion() method

--- src/app/design/frontend/Vendor/UI_Theme/Magento_Theme/web/js/view/parent.js
+++ src/app/design/frontend/Vendor/UI_Theme/Magento_Theme/web/js/view/parent.js
@@ -16,6 +16,8 @@ define(['uiCollection', 'uiLayout', 'uiEvents'], (uiCollection, uiLayout, uiEven
                 name: `child-${ new Date().getTime()}`,
                 component: 'Magento_Theme/js/view/child',
                 template: 'Magento_Theme/child',
+                // displayArea: this.elems.getLength() % 2 ? 'right':'left'  
+                displayArea: new Date().getTime() % 2 ? 'right' : 'left'
             }]);
         },
 
